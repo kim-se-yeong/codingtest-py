04a6598a14fe7d078f79ee5890e65c62af470e1e
kim-se-yeong <kimseyeong97@gmail.com>
2024-09-27 17:53:03 +0900

    프로그래머스 Lv1 풀기

---
+++ programmers/Lv1/대충만든자판.py
@@ -0,0 +1,31 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/160586
+
+def solution(keymap, targets):
+    answer = []
+    dic = {}
+    
+    for key in keymap:
+        # enumerate 함수를 이용해서 인덱스를 사용한다.
+        for idx, char in enumerate(key):
+            # 만약 dic에 char이 없으면 추가한다. key: char, value: idx + 1
+            if char not in dic:
+                dic[char] = idx + 1
+            else:
+                dic[char] = min(dic[char], idx + 1)
+        
+    for target in targets:
+        count = 0
+        
+        for char in target:
+            # dic에 char이 있으면 값을 가져와 count에 더한다.
+            if char in dic:
+                count += dic[char]
+            else:
+                count = -1
+                break
+        answer.append(count)
+    return answer
+
+keymap = ["ABACD", "BCEFD"]
+targets = ["ABCD","AABB"]
+print(solution(keymap, targets))
